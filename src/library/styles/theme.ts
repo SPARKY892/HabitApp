f22f2d57dad7736558c5afa30935aa5384a375bd
Mark <marksmellie892@gmail.com>
2024-03-19 16:56:54 +0000

    Prettier formatting

--- src/library/styles/theme.ts
+++ src/library/styles/theme.ts
@@ -1,45 +1,54 @@
-import theme from 'styled-theming';
-import { darken, lighten } from '@library/utils/colours';
-import { neutral } from './colours';
+import theme from "styled-theming";
+import { darken, lighten } from "@library/utils/colours";
+import { neutral } from "./colours";
 
-const card = theme('theme', {
+const card = theme("theme", {
   light: neutral.white,
   dark: neutral.grey5,
 });
 
-const cardPressed = theme('theme', {
+const cardPressed = theme("theme", {
   light: darken(neutral.white, 0.2),
   dark: lighten(neutral.offBlack, 0.2),
 });
 
-const background = theme('theme', {
+const background = theme("theme", {
   light: neutral.offWhite,
   dark: neutral.offBlack,
 });
 
-const headerTextColour = theme('theme', {
+const headerTextColour = theme("theme", {
   light: neutral.black,
   dark: neutral.white,
 });
 
-const bodyTextColour = theme('theme', {
+const bodyTextColour = theme("theme", {
   light: neutral.grey4,
   dark: neutral.cream,
 });
 
-const detailTextColour = theme('theme', {
+const detailTextColour = theme("theme", {
   light: neutral.grey4,
   dark: neutral.grey2,
 });
 
-const icon = theme('theme', {
+const icon = theme("theme", {
   light: neutral.grey3,
   dark: neutral.grey2,
 });
 
-const inputTextColour = theme('theme', {
+const inputTextColour = theme("theme", {
   light: neutral.offBlack,
   dark: neutral.offWhite,
 });
 
-export { card, cardPressed, background, headerTextColour, bodyTextColour, detailTextColour, icon, inputTextColour };
+export {
+  card,
+  cardPressed,
+  background,
+  headerTextColour,
+  bodyTextColour,
+  detailTextColour,
+  icon,
+  inputTextColour,
+};
